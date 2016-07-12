696bcc07a8f9efb2316303420171757e7b3338aa
hnwy416 <hnwy416@126.com>
2016-07-12 14:16:37 +0800

    app-orm-models

---
+++ www/app.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import logging
+import asyncio
+from aiohttp import web
+
+logging.basicConfig(level=logging.INFO)
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    print(srv)
+    print(locals())
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
